db3d0dad1bce6edd9cb5f080ea0586ae4c8aaa7f
pytergeist <tompopeworks@gmail.com>
2025-01-08 16:51:27 +0000

    adding max_priority_queue implamentation

--- algorithms/queue/c++/max_priority_queue.cpp
+++ algorithms/queue/c++/max_priority_queue.cpp
@@ -7,10 +7,36 @@ using namespace std;
 
 class MaxPriorityQueue {
   public:
-    heap = MaxHeap()
+    MaxHeap heap;
     void enqueue(<int> item,  <int> priority) {
         tuple<int, int> priority_item;
-        priority_item = make_tuple(priority, item)
-        this->heap.push(priority_item)
+        priority_item = make_tuple(priority, item);
+        this->heap.push(priority_item);
     }
+
+    int dequeue() {
+      if (this->heap.heap.size() = 0) {
+        throw std::runtime_error("Queue is empty!");
+      }
+      priority, item = this->heap.pop();
+      return item
+    }
+
+    int peek () {
+      if (this->heap.heap.size() = 0) {
+        throw std::runtime_error("Queue is empty!");
+      }
+      priority, item = this->heap.peek();
+      return item
+    }
+
+    bool is_empty () {
+      if (this->heap.size = 0) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
 }
