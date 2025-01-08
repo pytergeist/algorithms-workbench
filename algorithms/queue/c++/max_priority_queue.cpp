483a699f29a86089f6f9e91b0d0903374324f7ba
pytergeist <tompopeworks@gmail.com>
2025-01-08 16:44:08 +0000

    Adding enqueue method to mpq.cpp

--- algorithms/queue/c++/max_priority_queue.cpp
+++ algorithms/queue/c++/max_priority_queue.cpp
@@ -1,9 +1,16 @@
 #include "max_heap.h"
 #include <iostream>
+#include <vector>
+#include <tuple>
 using namespace std;
 
 
-class PriorityQueue {
+class MaxPriorityQueue {
   public:
-
+    heap = MaxHeap()
+    void enqueue(<int> item,  <int> priority) {
+        tuple<int, int> priority_item;
+        priority_item = make_tuple(priority, item)
+        this->heap.push(priority_item)
+    }
 }
