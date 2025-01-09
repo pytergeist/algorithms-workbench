d0244a3a29871473b0096c75aa0e6bbe1977183b
pytergeist <tompopeworks@gmail.com>
2025-01-09 17:06:11 +0000

    clang formatting repo

--- algorithms/queue/c++/max_priority_queue.cpp
+++ algorithms/queue/c++/max_priority_queue.cpp
@@ -1,42 +1,39 @@
 #include "max_heap.h"
 #include <iostream>
-#include <vector>
 #include <tuple>
+#include <vector>
 using namespace std;
 
-
 class MaxPriorityQueue {
-  public:
-    MaxHeap heap;
-    void enqueue(<int> item,  <int> priority) {
-        tuple<int, int> priority_item;
-        priority_item = make_tuple(priority, item);
-        this->heap.push(priority_item);
-    }
+public:
+  MaxHeap heap;
+  void enqueue(<int> item, <int> priority) {
+    tuple<int, int> priority_item;
+    priority_item = make_tuple(priority, item);
+    this->heap.push(priority_item);
+  }
 
-    int dequeue() {
-      if (this->heap.heap.size() = 0) {
-        throw std::runtime_error("Queue is empty!");
-      }
-      priority, item = this->heap.pop();
-      return item
+  int dequeue() {
+    if (this->heap.heap.size() = 0) {
+      throw std::runtime_error("Queue is empty!");
     }
+    priority, item = this->heap.pop();
+    return item
+  }
 
-    int peek () {
-      if (this->heap.heap.size() = 0) {
-        throw std::runtime_error("Queue is empty!");
-      }
-      priority, item = this->heap.peek();
-      return item
+  int peek() {
+    if (this->heap.heap.size() = 0) {
+      throw std::runtime_error("Queue is empty!");
     }
+    priority, item = this->heap.peek();
+    return item
+  }
 
-    bool is_empty () {
-      if (this->heap.size = 0) {
-        return true;
-      }
-      else {
-        return false;
-      }
+  bool is_empty() {
+    if (this->heap.size = 0) {
+      return true;
+    } else {
+      return false;
     }
-
+  }
 }
--- data_structures/trees/c++/max_heap.h
+++ data_structures/trees/c++/max_heap.h
@@ -1,33 +1,33 @@
 #ifndef MAX_HEAP_H
 #define MAX_HEAP_H
 
-#include <vector>
 #include <stdexcept>
+#include <vector>
 
 class MaxHeap {
 public:
-    // Member Variables
-    std::vector<int> heap;
-    size_t heap_size;
+  // Member Variables
+  std::vector<int> heap;
+  size_t heap_size;
 
-    // Constructor & Destructor
-    MaxHeap();
-    ~MaxHeap();
+  // Constructor & Destructor
+  MaxHeap();
+  ~MaxHeap();
 
-    // Public Member Functions
-    void build_heap(const std::vector<int>& values);
-    void push(int val);
-    int pop();
-    int peek() const;
-    void heap_sort();
+  // Public Member Functions
+  void build_heap(const std::vector<int> &values);
+  void push(int val);
+  int pop();
+  int peek() const;
+  void heap_sort();
 
 private:
-    // Helper Functions
-    int parent_idx(int idx) const;
-    int left_child_idx(int idx) const;
-    int right_child_idx(int idx) const;
-    void heapify_up(int idx);
-    void heapify_down(int idx, int heap_size = -1);
+  // Helper Functions
+  int parent_idx(int idx) const;
+  int left_child_idx(int idx) const;
+  int right_child_idx(int idx) const;
+  void heapify_up(int idx);
+  void heapify_down(int idx, int heap_size = -1);
 };
 
 #endif // MAX_HEAP_H
